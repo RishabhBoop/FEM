2e1dd50e48229bd1db9d1d495d044678106c7141
RishabhBoop <rishabh.tv@gmail.com>
2026-04-05 00:45:24 +0200

    finished aufgabe 2

--- uebungen.py
+++ uebungen.py
@@ -2,9 +2,16 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import time
+import scipy.sparse as sp
+import scipy.linalg as spla
+import json
+import os
+import pickle
 # --------------------------------------------------
 
-# d)
+TO_MB = 1024 * 1024
+
+# 1 d)
 def generate_t(pList: np.ndarray) -> np.ndarray:
     t = []
     pList = np.array(pList)
@@ -162,7 +169,7 @@ class Aufgabe1:
         plt.ylabel("f(x) = ln(x^2)")
         plt.legend()
         plt.grid(True, linestyle=':', alpha=0.7)
-
+    
     def aufgabe_g(self):
         print("------------ g) ------------")
         if not hasattr(self, 'y_p0'): self.aufgabe_f()
@@ -182,58 +189,326 @@ class Aufgabe1:
         exact_area = exaact_area_under_kurve(self.a, self.b)
         print(f"Exakte Fläche unter der Kurve: {exact_area:.4f}")
 
-        # Show all plots at the end
+
+    def execute_aufgabe(self, aufgabe = []):
+
+        aufgaben = {
+            'a': self.aufgabe_a,
+            'b': self.aufgabe_b,
+            'c': self.aufgabe_c,
+            'd': self.aufgabe_d,
+            'e': self.aufgabe_e,
+            'f': self.aufgabe_f,
+            'g': self.aufgabe_g
+        }
+
+        to_execute = aufgabe if aufgabe else aufgaben.keys()
+
+        for e in to_execute:
+            if e in aufgaben:
+                aufgaben[e]()
+            else:
+                print(f"Ungültige Aufgabe: {e}")
+
         plt.show()
 
 
-    def all_aufgaben(self):
-        self.aufgabe_a()
-        self.aufgabe_b()
-        self.aufgabe_c()
-        self.aufgabe_d()
-        self.aufgabe_e()
-        self.aufgabe_f()
-        self.aufgabe_g()
-
-
-def aufgabe_2():
-    # a)
-    dimMatrix = 10
-
-    matrA = np.random.rand(dimMatrix, dimMatrix)
-    vecB = np.random.rand(dimMatrix)
-
-    x = np.linalg.solve(matrA, vecB)
-    print("A =\n", matrA)
-    print("B =\n", vecB)
-    print("x =\n", x)
-
-    # b)
-    n_t = {}
-    for i in range(500, 15000, 1000):
-        matrA = np.random.rand(i, i)
-        vecB = np.random.rand(i)
-        start_time = time.time()
-        x = np.linalg.solve(matrA, vecB)
-        end_time = time.time()
-        print(f"Zeit für Dimension {i}: {end_time - start_time:.4f} Sekunden")
-        n_t[i] = end_time - start_time
+class Aufgabe2:
+    def __init__(self):
+        self.times_dense = None   # b)
+        self.times_banded = None  # c)
+        self.times_banded_special = None  # c) with scipy.solve_banded
+        self.times_sparse = None  # d)
     
-    plt.figure(figsize=(10, 6))
-    plt.plot(list(n_t.keys()), list(n_t.values()), marker='o')
-    plt.title("Zeit um NxN Matrix zu lösen")
-    plt.xlabel("Dimension N")
-    plt.ylabel("Zeit (Sekunden)")
-    plt.grid(True, linestyle=':', alpha=0.7)
+        self.memory_dense = None  # b)
+        self.memory_banded = None # c)
+        self.memory_banded_special = None # c) with scipy.solve_banded
+        self.memory_sparse = None # d)
 
 
+    def messe_zeit(self, matrix, vector):
+        start_time = time.time()
+        x = np.linalg.solve(matrix, vector)
+        end_time = time.time()
+        return end_time - start_time
 
-    plt.show()
+
+    def aufgabe_a(self):
+        print("------------ a) ------------")
+        dimMatrix = 10
+
+        matrA = np.random.rand(dimMatrix, dimMatrix)
+        vecB = np.random.rand(dimMatrix)
+
+        x = np.linalg.solve(matrA, vecB)
+        print("A =\n", matrA)
+        print("B =\n", vecB)
+        print("x =\n", x)
+
+
+    def aufgabe_b(self):
+        print("------------ b) ------------")
+        # Dense Matrix
+        self.times_dense = {}
+        self.memory_dense = {}
+        for i in range(500, 15000, 1000):
+            matrA = np.random.rand(i, i)
+            vecB = np.random.rand(i)
+            time_taken = self.messe_zeit(matrA, vecB)
+            arr_Size = matrA.nbytes / TO_MB
+            print(f"Zeit für N = {i}: {time_taken:.4f} Sekunden")
+            print(f"Memory für N = {i}: {arr_Size:.4f} MB")
+            self.times_dense[i] = time_taken
+            self.memory_dense[i] = arr_Size
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(list(self.times_dense.keys()), list(self.times_dense.values()), marker='x', label='Dense')
+        plt.title("Zeit um NxN Dense Matrix zu lösen")
+        plt.xlabel("Dimension N")
+        plt.ylabel("Zeit (Sekunden)")
+        plt.grid(True, linestyle=':', alpha=0.7)
+        plt.legend()
+
+
+    def aufgabe_c(self):
+        print("------------ c) ------------")
+        # Banded Matrix        
+        self.times_banded = {}
+        self.memory_banded = {}
+        for i in range(500, 15000, 1000):
+            oben = np.random.rand(i-1)
+            mitte = np.random.rand(i)
+            unten = np.random.rand(i-1)
+            matrA = np.diag(unten, -1) + np.diag(mitte, 0) + np.diag(oben, 1)
+            vecB = np.random.rand(i)
+            time_taken = self.messe_zeit(matrA, vecB)
+            arr_Size = matrA.nbytes / TO_MB
+            print(f"Zeit für N = {i}: {time_taken:.4f} Sekunden")
+            print(f"Memory für N = {i}: {arr_Size:.4f} MB")
+            self.times_banded[i] = time_taken
+            self.memory_banded[i] = arr_Size
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(list(self.times_banded.keys()), list(self.times_banded.values()), marker='x', label='Banded')
+        plt.title("Zeit um NxN Banded Matrix zu lösen")
+        plt.xlabel("Dimension N")
+        plt.ylabel("Zeit (Sekunden)")
+        plt.grid(True, linestyle=':', alpha=0.7)
+        plt.legend()
+
+
+    def aufgabe_c_special_solver(self):
+        print("------------ c) ------------")
+        # Banded Matrix with scipy.solve_banded       
+        self.times_banded_special = {}
+        self.memory_banded_special = {}
+        for i in range(500, 15000, 1000):
+            oben = np.random.rand(i-1)
+            mitte = np.random.rand(i)
+            unten = np.random.rand(i-1)
+
+            
+            ab = np.zeros((3, i))
+            ab[0, 1:] = oben  # obere Diagonale
+            ab[1, :] = mitte   # Hauptdiagonale
+            ab[2, :-1] = unten # untere Diagonale
+            vecB = np.random.rand(i)
+
+            start_time = time.time()
+            x = spla.solve_banded((1, 1), ab, vecB)
+            end_time = time.time()
+
+            time_taken = end_time - start_time
+            arr_Size = ab.nbytes / TO_MB
+            print(f"Zeit für N = {i}: {time_taken:.4f} Sekunden")
+            print(f"Memory für N = {i}: {arr_Size:.4f} MB")
+            self.times_banded_special[i] = time_taken
+            self.memory_banded_special[i] = arr_Size
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(list(self.times_banded_special.keys()), list(self.times_banded_special.values()), marker='x', label='Banded (with scipy.solve_banded)')
+        plt.title("Zeit um NxN Banded Matrix zu lösen")
+        plt.xlabel("Dimension N")
+        plt.ylabel("Zeit (Sekunden)")
+        plt.grid(True, linestyle=':', alpha=0.7)
+        plt.legend()
+
+
+    def aufgabe_d(self):
+        if self.times_dense is None: self.aufgabe_b()
+        if self.times_banded is None: self.aufgabe_c()
+
+        print("------------ d) ------------")
+        # Sparse Matrix
+        self.times_sparse = {}
+        self.memory_sparse = {}
+        for i in range(500, 15000, 1000):
+            oben = np.random.rand(i-1)
+            mitte = np.random.rand(i)
+            unten = np.random.rand(i-1)
+
+            matrA = sp.lil_matrix((i, i))
+            matrA.setdiag(mitte, 0)
+            matrA.setdiag(oben, 1)
+            matrA.setdiag(unten, -1)
+            matrA = matrA.tocsr()  
+
+            vecB = np.random.rand(i)
+
+            start_time = time.time()
+            x = sp.linalg.spsolve(matrA, vecB)
+            end_time = time.time()
+
+            time_taken = end_time - start_time
+            arr_Size = (matrA.data.nbytes + matrA.indices.nbytes + matrA.indptr.nbytes) / TO_MB
+            print(f"Zeit für N = {i}: {time_taken:.4f} Sekunden")
+            print(f"Memory für N = {i}: {arr_Size:.4f} MB")
+            self.times_sparse[i] = time_taken
+            self.memory_sparse[i] = arr_Size
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(list(self.times_sparse.keys()), list(self.times_sparse.values()), marker='x', label='Sparse')
+        plt.title("Zeit um NxN Sparse Matrix zu lösen")
+        plt.xlabel("Dimension N")
+        plt.ylabel("Zeit (Sekunden)")
+        plt.grid(True, linestyle=':', alpha=0.7)
+        plt.legend()
+
+        plt.figure(figsize=(10, 6))
+        plt.scatter(list(self.times_sparse.keys()), list(self.times_sparse.values()), marker='x', label='Sparse')
+        plt.scatter(list(self.times_dense.keys()), list(self.times_dense.values()), marker='x', label='Dense')
+        plt.scatter(list(self.times_banded.keys()), list(self.times_banded.values()), marker='x', label='Banded')
+        plt.title("Zeit um NxN Matrix zu lösen")
+        plt.xlabel("Dimension N")
+        plt.ylabel("Zeit (Sekunden)")
+        plt.grid(True, linestyle=':', alpha=0.7)
+        plt.legend()
+
+
+    def aufgabe_e(self):
+        if self.times_sparse is None: self.aufgabe_d()
+
+
+        print("------------ e) ------------")
+        dense_times = np.array(list(self.times_dense.values()))
+        banded_times = np.array(list(self.times_banded.values()))
+        sparse_times = np.array(list(self.times_sparse.values()))
+
+        nds = np.array(list(self.times_dense.keys()))
+
+        # fit to polynom (ax² + bx + c) in log-log space
+        dense = np.polyfit(np.log(nds), np.log(dense_times), 1)
+        banded = np.polyfit(np.log(nds), np.log(banded_times), 1)
+        sparse = np.polyfit(np.log(nds), np.log(sparse_times), 1)
+
+        y_fit_dense = np.exp(dense[1]) * nds**dense[0]
+        y_fit_banded = np.exp(banded[1]) * nds**banded[0]
+        y_fit_sparse = np.exp(sparse[1]) * nds**sparse[0]
+
+        plt.figure(figsize=(10, 6))
+        plt.scatter(list(self.times_sparse.keys()), list(self.times_sparse.values()), marker='x', label='Sparse', color='orange')
+        plt.scatter(list(self.times_dense.keys()), list(self.times_dense.values()), marker='x', label='Dense', color='blue')
+        plt.scatter(list(self.times_banded.keys()), list(self.times_banded.values()), marker='x', label='Banded', color='green')
+        plt.plot(nds, y_fit_dense, label=f'Dense Fit: O(N^{dense[0]:.2f})', linestyle='--', color='blue')
+        plt.plot(nds, y_fit_banded, label=f'Banded Fit: O(N^{banded[0]:.2f})', linestyle='--', color='green')
+        plt.plot(nds, y_fit_sparse, label=f'Sparse Fit: O(N^{sparse[0]:.2f})', linestyle='--', color='orange')    
+        plt.title("Zeit um NxN Matrix zu lösen mit Fit")
+        plt.xlabel("Dimension N")
+        plt.ylabel("Zeit (Sekunden)")
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.grid(True, linestyle=':', alpha=0.7)
+        plt.legend()
+
+        print(f"Dense: O(N^{dense[0]:.2f})")
+        print(f"Banded: O(N^{banded[0]:.2f})")
+        print(f"Sparse: O(N^{sparse[0]:.2f})")
+
+
+    def aufgabe_f(self):
+        if self.times_sparse is None: self.aufgabe_d()
+
+        print("------------ f) ------------")
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(list(self.memory_dense.keys()), list(self.memory_dense.values()), marker='x', label='Dense')
+        plt.plot(list(self.memory_banded.keys()), list(self.memory_banded.values()), marker='x', label='Banded')
+        plt.plot(list(self.memory_sparse.keys()), list(self.memory_sparse.values()), marker='x', label='Sparse')
+        plt.title("Speicherverbrauch für NxN Matrix")
+        plt.xlabel("Dimension N")
+        plt.ylabel("Speicherverbrauch (MB)")
+        plt.grid(True, linestyle=':', alpha=0.7)
+        plt.legend()
+
+
+    def load_data(self):
+        filename = "messungen_uebungen_a2.pkl"
+        if os.path.exists(filename):
+            with open(filename, 'rb') as f:
+                data = pickle.load(f)
+                self.times_dense = data.get("times_dense", {})
+                self.times_banded = data.get("times_banded", {})
+                self.times_banded_special = data.get("times_banded_special", {})
+                self.times_sparse = data.get("times_sparse", {})
+                self.memory_dense = data.get("memory_dense", {})
+                self.memory_banded = data.get("memory_banded", {})
+                self.memory_banded_special = data.get("memory_banded_special", {})
+                self.memory_sparse = data.get("memory_sparse", {})
+            print("Data successfully loaded from file!")
+        else:
+            print("No existing file found, recalculating data...")
+
+    def save_data(self):
+        filename = "messungen_uebungen_a2.pkl"
+        data = {
+            "times_dense": self.times_dense,
+            "times_banded": self.times_banded,
+            "times_banded_special": self.times_banded_special,
+            "times_sparse": self.times_sparse,
+            "memory_dense": self.memory_dense,
+            "memory_banded": self.memory_banded,
+            "memory_banded_special": self.memory_banded_special,
+            "memory_sparse": self.memory_sparse
+        }
+        
+        with open(filename, 'wb') as f:
+            pickle.dump(data, f)
+        print(f"Data successfully saved to {filename}!")
+
+
+    def execute_aufgabe(self, aufgabe = []):
+
+        aufgaben = {
+            'a': self.aufgabe_a,
+            'b': self.aufgabe_b,
+            'c': self.aufgabe_c,
+            'z': self.aufgabe_c_special_solver,
+            'd': self.aufgabe_d,
+            'e': self.aufgabe_e,
+            'f': self.aufgabe_f,
+        }
+        to_execute = aufgabe if aufgabe else aufgaben.keys()
+
+        self.load_data()
+
+        for e in to_execute:
+            if e in aufgaben:
+                aufgaben[e]()
+            else:
+                print(f"Ungültige Aufgabe: {e}")
+
+        self.save_data()
+
+        plt.show()
+
+        
 
 
 
 
 if __name__ == "__main__":
     # a1 = Aufgabe1()
-    # a1.all_aufgaben()
-    aufgabe_1()
+    # a1.execute_aufgabe()
+
+    a2 = Aufgabe2()
+    a2.execute_aufgabe(['e'])
