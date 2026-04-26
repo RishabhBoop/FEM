67fbad71b42a66bb6ebf5e72f1c6b1b5e882cabc
RishabhBoop <rishabh.tv@gmail.com>
2026-04-26 21:38:20 +0200

    added two new functions

--- src_cpp/binding.cpp
+++ src_cpp/binding.cpp
@@ -28,5 +28,6 @@ PYBIND11_MODULE(fem_cpp, m)
         .def("reconstruct_solution", &FEM_1D::reconstruct_solution, "Reconstruct the full solution vector Sol from the free nodes and Dirichlet boundary conditions")
     
         .def("full_solve", &FEM_1D::full_solve, "Run the full FEM solve process")
-        .def("get_Solution", &FEM_1D::get_Solution, "Get the computed solution vector Sol as a NumPy array");
+        .def("get_Solution", &FEM_1D::get_Solution, "Get the computed solution vector Sol as a NumPy array")
+        .def("validate_sol", &FEM_1D::validate_sol, "Validate the computed solution against an analytical solution at the nodes, returning a vector of errors");
 }
