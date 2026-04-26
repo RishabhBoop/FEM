75bf179d855a292c8a63553a2058f2c85128c98f
RishabhBoop <rishabh.tv@gmail.com>
2026-04-26 21:40:01 +0200

    test with robin RW

--- src_cpp/main.cpp
+++ src_cpp/main.cpp
@@ -4,19 +4,25 @@ using namespace std;
 
 int main()
 {
-    auto alpha = [](double x) { return pow(x, 2); };
-    auto beta  = [](double x) { return x; };
-    auto f     = [](double x) { return -pow(x, 3); };
-    auto phi   = [](double x) { return (x < 1.5) ? 2.0 : 6.0; };
-    auto gamma = [](double x) { return 0.0; };
-    auto q     = [](double x) { return 0.0; };
+    auto alpha = [](double x)
+    { return pow(x, 2); };
+    auto beta = [](double x)
+    { return x; };
+    auto f = [](double x)
+    { return -pow(x, 3); };
+    auto phi = [](double x)
+    { return (x < 1.5) ? 2.0 : 6.0; };
+    auto gamma = [](double x)
+    { return (abs(x - 1.0) < 1e-10) ? 3.0 : 6.0; };
+    auto q = [](double x)
+    { return (abs(x - 1.0) < 1e-10) ? 4.0 : 7.0; };
 
-    Vector xD(2);
-    xD << 1.0, 2.0;   // Dirichlet boundary nodes at x=1 and x=2
-    Vector xR(0); // No Robin boundary nodes
+    Vector xD(0);
+    Vector xR(2);   // No Robin boundary nodes
+    xR << 1.0, 2.0; // Robin boundary nodes
     Vector plist(5);
     plist << 1.75, 2.0, 1.25, 1.0, 1.5; // Nodes
-    
+
     FEM_1D TST(
         xD,
         xR,
@@ -26,8 +32,7 @@ int main()
         f,
         phi,
         gamma,
-        q
-    );
+        q);
     TST.full_solve();
     TST.print_solution();
 
