67c583977519a8453088d42bee3aa0c5e21ede47
RishabhBoop <rishabh.tv@gmail.com>
2026-05-28 17:03:29 +0200

    cleaned up and updated a bit

--- uebungen/fem2d_skript.py
+++ uebungen/fem2d_skript.py
@@ -1,3 +1,5 @@
+from time import time
+
 import numpy as np
 import matplotlib.pyplot as plt
 from numba import cfunc, float64
@@ -12,7 +14,7 @@ import fem_cpp
 
 from helper_funcs.colors import Colors as colors
 from helper_funcs.visualizations import visualize_solution, print_timings
-
+from helper_funcs.mesh import get_plist_tlist_from_gmsh, get_boundaries
 
 @cfunc(float64(float64, float64))
 def alpha1(x, y):
@@ -47,6 +49,7 @@ def gamma(x, y):
 @cfunc(float64(float64, float64))
 def g(x, y):
     return 0.0
+
 
 def gen_mesh():
     gmsh.initialize()
@@ -83,8 +86,7 @@ def gen_mesh():
     # Embed points (dim 0) with tags [5, 6] into the surface (dim 2) with tag 1.
     gmsh.model.mesh.embed(0, [5, 6], 2, 1)
 
-
-    # add boundary 
+    # add boundary
     gmsh.model.addPhysicalGroup(1, [1, 2, 3, 4], 99)
 
     # Save all elements regardless of physical groups
@@ -92,65 +94,52 @@ def gen_mesh():
 
     # sync
     gmsh.model.mesh.generate(2)
-    
+
     gmsh.write(f"{name}.msh")
 
     # gmsh.fltk.run()  # Optional: to visualize the mesh
     gmsh.finalize()
 
 
-
-def get_plist_tlist_from_gmsh(meshname):
-    gmsh.initialize()
-
-    gmsh.open(meshname)
-    nodeTags, coords, parametricCoord = gmsh.model.mesh.getNodes()
-    plist = np.array(coords).reshape(-1, 3)[:,:2]
-    tp,nm,el = gmsh.model.mesh.getElements(2, -1)
-    tlist = np.array(el[0].reshape(-1, 3))-1
-
-    gmsh.finalize()
-    return plist, tlist
-
-def get_boundaries(meshname):
-    gmsh.initialize()
-    gmsh.open(meshname)
-    # Get all boundary nodes (those on the edges of the surface)
-    entities = gmsh.model.getEntitiesForPhysicalGroup(1, 99)
-    # print("Entities in Physical Group 99 (Boundary Edges):", entities)
-    # Get nodes from those entities
-    boundary_node_tags = set()
-    for edge_tag in entities:
-        nodes = gmsh.model.mesh.getNodes(1, edge_tag)[0]
-        boundary_node_tags.update(nodes)
-    
-    dr = np.array(sorted(boundary_node_tags)).astype(int) - 1
-
-    gmsh.finalize()
-    return dr
-
 def main():
     gen_mesh()
+    print(colors.SUCCESS + "Mesh generated successfully." + colors.RESET)
+    
+    t0 = time()
     plist, tlist = get_plist_tlist_from_gmsh("fem2d_skript.msh")
+    t1 = time()
+    print(colors.INFO + "Number of points (plist):", len(plist), colors.RESET)
+    print(colors.INFO + "Number of Triangles (tlist):", len(tlist), colors.RESET)
+    plist_time = t1 - t0
+    
+    t0 = time()
     dr = get_boundaries("fem2d_skript.msh")
-    print("dr:", dr)
-    # dr = [0, 1, 2, 3]  # dirichlet boundary nodes (all 4 corners)
-    xD = plist[dr, 0]  # x-koordinaten der dirichlet boundary conditions
+    t1 = time()
+    boundary_time = t1 - t0
+    print(colors.SUCCESS + "Boundary nodes extracted successfully." + colors.RESET)
+
+    xD = plist[dr, 0]
     xR = []
 
     fem_solver = fem_cpp.FEM_2D(xD, xR, plist, tlist, alpha1, alpha2, beta, f, phi, gamma, g)
+    
     try:
         timings = fem_solver.full_solve()
         sol = fem_solver.get_Solution()
-        print(colors.BOLDGREEN + "FEM solve completed successfully." + colors.RESET)
+        print(colors.SUCCESS + "FEM solve completed successfully." + colors.RESET)
     except Exception as e:
-        print(colors.BOLDRED + "Error during FEM solve: " + str(e) + colors.RESET)
+        print(colors.FAIL + "Error during FEM solve: " + str(e) + colors.RESET)
         return
+    
+    timings.insert(0, ("gen_plist_tlist", plist_time))
+    timings.insert(1, ("get_boundaries", boundary_time))
 
+    print_timings(timings, "FEM 2D Skript Timings", len(plist), len(tlist), False, "CPP")
     visualize_solution(plist, tlist, sol, True, "Lösung 2D", False, "CPP", f"Loesung2D_{len(plist)}points_sol.png")
     visualize_solution(plist, tlist, sol, False, "Lösung 2D", False, "CPP", f"Loesung2D_{len(plist)}points_sol.png")
 
     plt.show()
+
 
 def mesh():
     gen_mesh()
